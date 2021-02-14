ec044a9a3efef1982cfacfc81c7690b0bfaba5dc
manabu2222 <diamond.mind.trip@gmail.com>
2021-02-15 00:21:00 +0900

    Task. 5-4 application.rb の設定【環境構築】

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,19 @@ module WonderfulEditor
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
-    # Don't generate system test files.
+    config.generators do |g|
+      g.template_engine false
+      g.javascripts false
+      g.stylesheets false
+      g.helper false
+      g.test_framework :rspec,
+                       view_specs: false,
+                       routing_specs: false,
+                       helper_specs: false,
+                       controller_specs: false,
+                       request_specs: true
+    end
+    config.api_only = true
     config.generators.system_tests = nil
   end
 end
