0d8cd1d414f25a4b11b0fb1ee48f30841c5f1a00
manabu2222 <diamond.mind.trip@gmail.com>
2021-04-18 09:59:56 +0900

    rubocop実行時のエラー修正

--- db/migrate/20210413125606_add_status_to_articles.rb
+++ db/migrate/20210413125606_add_status_to_articles.rb
@@ -1,10 +1,10 @@
 class AddStatusToArticles < ActiveRecord::Migration[6.0]
-  #変更内容
+  # 変更内容
   def up
     add_column :articles, :status, :string, default: "draft"
   end
 
-  #変更前の状態
+  # 変更前の状態
   def down
     add_column :articles, :status, :int, default: "draft"
   end
