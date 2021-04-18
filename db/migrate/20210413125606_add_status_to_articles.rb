c0a96a49751d520f402db9ec600f39b84dcdf070
manabu2222 <diamond.mind.trip@gmail.com>
2021-04-18 09:56:51 +0900

    Articleテーブルのstatusカラムの型をintからstringへ変更

--- db/migrate/20210413125606_add_status_to_articles.rb
+++ db/migrate/20210413125606_add_status_to_articles.rb
@@ -1,5 +1,11 @@
 class AddStatusToArticles < ActiveRecord::Migration[6.0]
-  def change
+  #変更内容
+  def up
+    add_column :articles, :status, :string, default: "draft"
+  end
+
+  #変更前の状態
+  def down
     add_column :articles, :status, :int, default: "draft"
   end
 end
