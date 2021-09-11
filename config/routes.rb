c15c61710b3cc4ec3e7d34e36a0107e17d486d5d
manabu2222 <diamond.mind.trip@gmail.com>
2021-09-11 18:07:48 +0900

    不要なコメント箇所を消去。

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   # reload 対策
   get "sign_up", to: "home#index"
   get "sign_in", to: "home#index"
-  # get "guest_sign_ins", to: "home#index"
   get "articles/new", to: "home#index"
   get "articles/draft", to: "home#index"
   get "articles/drafts/:id/edit", to: "home#index"
@@ -16,7 +15,6 @@ Rails.application.routes.draw do
     namespace :v1 do
       mount_devise_token_auth_for "User", at: "auth", controllers: {
         registrations: "api/v1/auth/registrations",
-        # guest_sign_ins: "api/v1/auth/guest_sign_ins",
       }
       namespace :articles do
         resources :drafts, only: [:index, :show]
