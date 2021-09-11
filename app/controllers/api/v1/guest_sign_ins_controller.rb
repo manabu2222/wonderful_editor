fdeb89461f0e4e6e664198ac7c8e04104f8ac061
manabu2222 <diamond.mind.trip@gmail.com>
2021-09-11 18:14:26 +0900

    ゲストユーザー作成判定処理を追加。

--- app/controllers/api/v1/guest_sign_ins_controller.rb
+++ app/controllers/api/v1/guest_sign_ins_controller.rb
@@ -1,7 +1,13 @@
 module Api::V1
   class GuestSignInsController < BaseApiController
+
+    # ゲストユーザー専用のメールアドレスが DB 内に登録されているか否かの確認
+    # 登録されていないとき、メールアドレス（guest@example.com）パスワード（ランダム）で
+    # ゲストユーザー生成
     def create
-      binding.pry
+      user = User.find_or_create_by!(name: 'guest',email: 'guest@example.com') do |user|
+        user.password = SecureRandom.urlsafe_base64
+      end
     end
   end
 end
