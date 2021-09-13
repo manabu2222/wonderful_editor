710fa7c6c808da66ca84c61d4779e06c57344013
manabu2222 <diamond.mind.trip@gmail.com>
2021-09-14 00:36:56 +0900

    ゲストログインボタン押下時の処理追加

--- app/controllers/api/v1/guest_sign_ins_controller.rb
+++ app/controllers/api/v1/guest_sign_ins_controller.rb
@@ -8,6 +8,10 @@ module Api::V1
       user = User.find_or_create_by!(name: 'guest',email: 'guest@example.com') do |user|
         user.password = SecureRandom.urlsafe_base64
       end
+      tokens = user.create_new_auth_token
+      headers["access-token"] = tokens["access-token"]
+      headers["uid"] = tokens["uid"]
+      headers["client"] = tokens["client"]
     end
   end
 end
